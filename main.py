819a8a17e6f452c0d3cdc649ab74ba5b0a36193e
Alex <dev.alex.ya@gmail.com>
2019-04-23 16:08:53 +0300

    attempt to fix

--- main.py
+++ main.py
@@ -99,10 +99,10 @@ def run():
     solutions = bzu.split('\n\n')
 
     #передаем боту токен подключения
-    bot = telegram.Bot(token=token)
+    bot = telegram.Bot(token = "bot" + token)
 
     #инициализируем объект-обновлятор
-    updater = Updater(token=token)
+    updater = Updater(token = "bot" + token)
 
     #инициализируем систему диспетчиризации
     dispatcher = updater.dispatcher
